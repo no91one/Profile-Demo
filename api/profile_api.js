6ad0b48f37a4a3338e91b55ec1f577e98d0636f2
Naveen Singh <iamno91one@gmail.com>
2020-12-12 14:33:52 +0530

    Created Users View

--- api/profile_api.js
+++ api/profile_api.js
@@ -4,31 +4,47 @@ module.exports.create = async function (req, res) {
         var Avatar = User.avatarPath + '/' + req.file.filename;
     }
     try {
-            await User.create({
-                name: req.body.name,
-                age: req.body.age,
-                rollno: req.body.rollno,
-                score: req.body.score,
-                email: req.body.email,
-                avatar: Avatar
-            });
-            return res.json(200, {
-                message: "Profile created succesfully",
-                data: {
-                    name: req.body.name,
-                    age: req.body.age,
-                    rollno: req.body.rollno,
-                    score: req.body.score,
-                    email: req.body.email,
-                    avatar: Avatar
-                }
-            }
-            );
-            
+        await User.create({
+            name: req.body.name,
+            age: req.body.age,
+            rollno: req.body.rollno,
+            score: req.body.score,
+            email: req.body.email,
+            avatar: Avatar
+        });
+        return res.redirect('/users');
+        // return res.json(200, {
+        //     message: "Profile created succesfully",
+        //     data: {
+        //         name: req.body.name,
+        //         age: req.body.age,
+        //         rollno: req.body.rollno,
+        //         score: req.body.score,
+        //         email: req.body.email,
+        //         avatar: Avatar
+        //     }
+        // }
+        // );
+
     } catch (err) {
         return res.json(200, {
             message: "Unable to create Profile"
         });
     }
 
+}
+module.exports.users = function (req, res) {
+
+    User.find({}, function (err, users) {
+        if (err) {
+            return res.json(200, {
+                message: "Unable to render Users"
+            });
+        }
+        console.log(users);
+        return res.render('Allusers', {
+            title: "Users",
+            users:users
+        });
+    });
 }
--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,7 @@ const profileApi = require('../api/profile_api');
 
 const User = require('../model/user');
 
+router.get('/', profileApi.users);
 router.post('/create', User.uploadedAvatar, profileApi.create);
 
 module.exports = router;
